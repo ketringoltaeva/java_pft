31b2570460f6eba9dee1760d33ace7cbe4062338
ketringoltaeva <k.goltaeva@gmail.com>
2019-05-02 23:24:21 +0500

    Создание класса Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    double x; // абсцисса точки
+    double y; // ордината точки
+    Point(double a, double b) {
+        x = a;
+        y = b;
+    }
+  //  попытка поработать с методом
+    //  public double distance () {
+
+      // double this.l = Math.sqrt( Math.pow((this.x - this.x),2) + Math.pow((this.y - this.y),2));
+        //return this.l;
+    //}
+}
