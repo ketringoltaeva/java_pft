3284b08ffccc1870f71082b8fc19cc45c8791d24
ketringoltaeva <k.goltaeva@gmail.com>
2019-05-03 13:05:44 +0500

    Создание класса Ractangle

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Ractangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Ractangle {
+
+    public double a;
+    public double b;
+
+    public Ractangle(double a, double b) {
+        this.a = a;
+        this.b= b;
+    }
+    public double area() {
+        return this.a * this.b;
+    }
+}
