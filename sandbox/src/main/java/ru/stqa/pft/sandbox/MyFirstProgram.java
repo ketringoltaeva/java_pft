260fbfc8a1b037a2f7d36ab49a00fc80566a29d0
ketringoltaeva <k.goltaeva@gmail.com>
2019-05-20 21:31:48 +0500

    Использование метода public double distance вместо функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,7 +12,7 @@ public class MyFirstProgram {
         Point p1 = new Point(3, 4);
         Point p2 = new Point(5, 7);
 
-        System.out.println("Расстояние между точками " + "p1(" + p1.x + ";" + p1.y + ") и " + "p2(" + p2.x + ";" + p2.y + ") = " + p1.distance(p1, p2));
+        System.out.println("Расстояние между точками " + "p1(" + p1.x + ";" + p1.y + ") и " + "p2(" + p2.x + ";" + p2.y + ") = " + p1.distance(p2));
 
         Square s = new Square(5);
         System.out.println("Площдь квадрата со стороной " + s.l + " = " + s.area());
